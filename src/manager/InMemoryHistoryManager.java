0bad4cee4861cc108e6afdee7f5dbdc1c83883a8
Polina <cherkaschenkopo@gmail.com>
2023-02-23 09:15:01 +0300

    fix: внесла изменения в методе linkLast(Task task) и removeNode(Node node) fix: убрала параметризацию <Task>

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,7 +2,10 @@ package manager;
 
 import task.Task;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final Map<Integer, Node> nodeIds = new HashMap<>();
